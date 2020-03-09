6f1a0341f0cf9e1c22e50a81f7201d52a4f047d8
hsienfu <hsienfu.tseng@gmail.com>
2020-03-09 11:19:17 +0800

    fixed typo

--- index.js
+++ index.js
@@ -4,10 +4,9 @@
  * Initiator: EastBlock
  */
 const elliptic = require("elliptic");
-const {bytes,nat,rlp,hash} = require('eth-lib');
-const Bytes = bytes;
+const {Bytes,Nat,RLP,Hash} = require('eth-lib');
 const secp256k1 = new (elliptic.ec)("secp256k1"); // eslint-disable-line
-const {keccak256, keccak256s} = hash;
+const {keccak256, keccak256s} = Hash;
 const { SHA3 } = require('sha3');
 const BigNumber = require('bignumber.js');
 const crypto = require('crypto');
@@ -24,22 +23,22 @@ class Signer {
             privateKey: outerHex
         }
     }
-    
+
 
     genTxHash(tx){
-        
+
         let buf_target=Util.encode(Util.toBytesPadded(tx.target,32));
-        
+
         let buf_value = Util.encode(Util.BigNumber2Bytes(tx.value));
-        
+
         let buf_nonce = Util.encode(Util.trimLeading(Util.int2Bytes(tx.nonce)));
-        
+
         let buf_gas_limit = Util.encode(Util.trimLeading(Util.int2Bytes(tx.gas)));
-        
+
         let buf_gas_price = Util.encode(Util.trimLeading(Util.int2Bytes(tx.gasPrice)));
-        
+
         let buf_type = Buffer.from([tx.type]);
-        
+
         let buf_data;
         if (tx.data) {
             buf_data = Util.encode(Buffer.from(tx.data,'hex'));
@@ -47,12 +46,12 @@ class Signer {
         else {
             buf_data = Util.encode(Buffer.alloc(0));
         }
-        
+
 
         let len = (buf_target.length+buf_value.length+buf_nonce.length+buf_gas_limit.length+buf_gas_price.length+buf_type.length+buf_data.length);
-        
+
         let result = Buffer.concat([buf_target,buf_value,buf_nonce,buf_gas_limit,buf_gas_price,buf_type,buf_data],);
-        
+
         let hash = crypto.createHash('sha256');
         hash.update(result);
         return hash.digest('hex');
@@ -60,17 +59,17 @@ class Signer {
 
     sign(hashStr, privateKey){
         let hashBuf = Buffer.from(hashStr,'hex');
-        
+
         let buf=privateKey.startsWith('0x')?Buffer.from(privateKey.slice(2),'hex'):Buffer.from(privateKey,'hex');
-        
+
         let signature= secp256k1.keyFromPrivate(buf).sign(hashBuf, {canonical: true});
         let buf_r = Util.toBytesPadded(signature.r.toString(16),32);
-        
-        
+
+
         let buf_s = Util.toBytesPadded(signature.s.toString(16),32);
-        
+
         let buf_v = Buffer.from([signature.recoveryParam]);
-        
+
         let ret= Buffer.concat([buf_r,buf_s,buf_v],65);
         return '0x'+ret.toString('hex')
     }
@@ -81,7 +80,7 @@ class Signer {
         let v = sign.substring(128,130);
         return {r,s,v}
     }
-    
+
     serializeTX(tx){
        return `"{\\"target\\": \\"${tx.target}\\",\\"value\\": ${tx.value.toString()},\\"nonce\\": ${tx.nonce},\\"gas\\":${tx.gas},\\"gasprice\\":${tx.gasPrice},\\"tx_type\\":${tx.type},\\"data\\": ${tx.data||null},\\"sign\\": \\"${tx.sign}\\"}"`;
     }
@@ -106,7 +105,7 @@ class Signer {
         const buffer = Buffer.from(privateKey.slice(2), "hex");
         const ecKey = secp256k1.keyFromPrivate(buffer);
         const publicKey = ecKey.getPublic(false, 'hex').slice(2);
-        return this.getAddressFromPublicKey(publicKey);   
+        return this.getAddressFromPublicKey(publicKey);
     }
 
     recoverAddress(hash, vrs){
